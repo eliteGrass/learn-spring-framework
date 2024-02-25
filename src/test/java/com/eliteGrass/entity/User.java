573fe3f84e5848e4f7c23f1042e454814306ffc2
eliteGrass <15688179817@163.com>
2024-02-25 20:53:11 +0800

    源码构建

---
+++ src/test/java/com/eliteGrass/entity/User.java
@@ -0,0 +1,11 @@
+package com.eliteGrass.entity;
+
+/**
+ * @Author: eliteGrass
+ * @CreateTime: 2024-02-25  20:41
+ * @Description:
+ */
+public class User {
+	private String userName;
+	private Integer age;
+}
